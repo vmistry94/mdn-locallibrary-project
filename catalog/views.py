3c5aebcfa9d96e2dfee39c7eadb5b0e03c3ca21b
Viral <vmistry94@gmail.com>
2022-07-24 23:38:39 +0100

    view added

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+def index(request):
+
+    # Generate some high-level counts
+    num_books = Book.objects.count()
+    num_instances = BookInstance.objects.count()
+    num_authors = Author.objects.count()
+
+    # Available books
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'index.html', context=context)
