1f85c30bd7cf9ed7fc60b22672acff54a7a3285b
Viral <vmistry94@gmail.com>
2022-08-01 15:53:43 +0100

    added session count

--- catalog/views.py
+++ catalog/views.py
@@ -12,11 +12,16 @@ def index(request):
     # Available books
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
 
+    # Session data - number of visits to this view
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
     return render(request, 'index.html', context=context)
